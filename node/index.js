32d1c19c538e993cf5616a629dd8ac2baf3735e6
polybean <polybean@gmail.com>
2019-11-15 14:24:27 +0800

    Integrate createWorkflowInstance & publishMessage

--- node/index.js
+++ node/index.js
@@ -1,22 +1,36 @@
 const { ZBClient } = require("zeebe-node");
 
 const zbc = new ZBClient("localhost:26500");
+const uuid = require("uuid/v4");
 
-zbc.createWorker("my-first-worker", "log", (job, complete) => {
+zbc.createWorker(uuid(), "create-claim-request", async (job, complete) => {
+  console.log(job.variables);
+  complete.success();
+
+  await zbc.publishMessage({
+    correlationKey: job.variables.correlationKey,
+    messageId: uuid(),
+    name: "claim-created",
+    variables: { valueToAddToWorkflowVariables: "here", status: "PROCESSED" },
+    timeToLive: 10000
+  });
+});
+
+zbc.createWorker(uuid(), "accept-reject-claim-request", (job, complete) => {
   console.log(job);
-  // complete.success({
-  //   approved: true,
-  //   customerId: 99
-  // });
   complete.success();
 });
 
-// zbc.createWorker("my-second-worker", "fetch-items", (job, complete) => {
-//   console.log(job);
-//   complete.success();
-// });
-
-// zbc.createWorker("my-third-worker", "ship-parcel", (job, complete) => {
-//   console.log(job);
-//   complete.success();
-// });
+(async () => {
+  const result = await zbc.createWorkflowInstance(
+    "claim-management",
+    {
+      testData: "something",
+      correlationKey: uuid()
+    },
+    {
+      version: 2
+    }
+  );
+  console.log(result);
+})();
