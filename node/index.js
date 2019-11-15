6ce0753b11d29fc16b714840a03a4d40def0bd2a
polybean <polybean@gmail.com>
2019-11-15 14:51:26 +0800

    Make exclusive gateway work

--- node/index.js
+++ node/index.js
@@ -4,6 +4,7 @@ const zbc = new ZBClient("localhost:26500");
 const uuid = require("uuid/v4");
 
 zbc.createWorker(uuid(), "create-claim-request", async (job, complete) => {
+  console.log("create-claim-request");
   console.log(job.variables);
   complete.success();
 
@@ -17,6 +18,21 @@ zbc.createWorker(uuid(), "create-claim-request", async (job, complete) => {
 });
 
 zbc.createWorker(uuid(), "accept-reject-claim-request", (job, complete) => {
+  console.log("accept-reject-claim-request");
+  console.log(job);
+  complete.success({
+    approved: true
+  });
+});
+
+zbc.createWorker(uuid(), "update-dispute-case-status", (job, complete) => {
+  console.log("update-dispute-case-status");
+  console.log(job);
+  complete.success();
+});
+
+zbc.createWorker(uuid(), "create-claim", (job, complete) => {
+  console.log("create-claim");
   console.log(job);
   complete.success();
 });
@@ -29,7 +45,7 @@ zbc.createWorker(uuid(), "accept-reject-claim-request", (job, complete) => {
       correlationKey: uuid()
     },
     {
-      version: 2
+      version: 3
     }
   );
   console.log(result);
